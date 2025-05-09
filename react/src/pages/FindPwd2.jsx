279ed249d9f356ad8e22b0258716853f5a3b4247
ezen402-21 <t01040786601@gmail.com>
2025-05-09 15:53:05 +0900

    0509 15:52 회원가입 화면 중간 취합

---
+++ react/src/pages/FindPwd2.jsx
@@ -0,0 +1,31 @@
+import './FindPwd.css';
+
+function FindPwd2() {
+    return (
+        <div className="container">
+            <div className="header">
+
+            </div>
+
+            <div className="box">
+                <div className='keyimg'>
+                    <img className='keyimg1' src='img/key.png'/>
+                </div>
+                
+                <h1 className="title">New Password</h1>
+                
+                <form className="form">
+                    <label className="label">pw</label>
+                    <input type="text" id="name" className="text_box" />
+
+                    <label className="label">check pw</label>
+                    <input type="text" id="text" className="text_box" />
+            
+                    <button className="submit_btn">Reset Password</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default FindPwd2;
