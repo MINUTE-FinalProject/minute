dd32d3b4c7c812755a9b2e72dbfc309667aebf1d
hyunuk-gh <qkrgusdnr13@gmail.com>
2025-05-12 09:59:10 +0900

    #12 09:58 북마크 나브바 수정

--- react/src/pages/Bookmark/bookmark.jsx
+++ react/src/pages/Bookmark/bookmark.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import bookmarkStyle from './bookmark.module.css';
-
+import MypageNav from "../../components/MypageNavBar/MypageNav";
 
 const Bookmark = () => {
   return (
-    <div className={bookmarkStyle.bookmarkContainer}>
-      <aside className={bookmarkStyle.sidebar}>
-        <div className={bookmarkStyle.logo}><img src="/logo.png" alt="로고" /></div>
-        <div className={bookmarkStyle.menuIcon}>☰</div>
-      </aside>
+    <div className={bookmarkStyle.container}>
+      <div className={bookmarkStyle.navWrap}>
+        <MypageNav />
+      </div>
       <main className={bookmarkStyle.mainContent}>
         <header className={bookmarkStyle.header}>
           <h1>BOOKMARK</h1>
           <div className={bookmarkStyle.buttons}>
-            <button className={bookmarkStyle.btn}>추가1</button>
+            <button className={bookmarkStyle.btn}>추가</button>
             <button className={bookmarkStyle.btn}>삭제</button>
           </div>
         </header>
