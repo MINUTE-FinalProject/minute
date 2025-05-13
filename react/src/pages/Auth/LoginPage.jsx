0750d854a64579843554c2120600763957b024da
ezen402-21 <t01040786601@gmail.com>
2025-05-13 16:21:55 +0900

    #23 로그인 화면 이미지 수정, css 수정

--- react/src/pages/Auth/LoginPage.jsx
+++ react/src/pages/Auth/LoginPage.jsx
@@ -1,4 +1,4 @@
-import img from '../../assets/images/key.png';
+import img from '../../assets/images/loginimg.png';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import styles from "./LoginPage.module.css";
@@ -13,9 +13,11 @@ const LoginPage = () => {
       </div>
       <div className={styles.loginBox}>
         <h1 className={styles.logo}>MIN:UTE</h1>
+        <div className="formGroup">
         <input type="text" placeholder="UserName" className={styles.input} />
         <input type="password" placeholder="Password" className={styles.input} />
         <button className={styles.btn}>Login</button>
+        </div>
         <div className={styles.links}>
           <a href="#">아이디찾기</a> | <a href="#">비밀번호찾기</a> | <a href="#">회원가입</a>
         </div>
