2dc08f2a477d3621473f0b4f3da4cecfe72e3833
ezen402-21 <t01040786601@gmail.com>
2025-05-20 10:46:30 +0900

    0520 10:46 로그인페이지 로그인 박스 css 수정, 헤더 버튼 로그인, 로그아웃 상태 수정

--- react/src/components/Header/Header.jsx
+++ react/src/components/Header/Header.jsx
@@ -1,15 +1,30 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/images/LO3.png";
 import HamburgerMenu from "./HamburgerMenu";
 import HeaderStyle from "./Header.module.css";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  //토큰으로 로그인 상태 관리
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(!!token);
+  }, []);
 
   // 로고 클릭 시 메뉴바 닫기
   const handleLogoClick = () => {
     setIsOpen(false);
+  };
+
+  //로그아웃
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    window.location.href = "/"; // 로그아웃 후 홈으로 이동
   };
 
   return (
@@ -22,15 +37,25 @@ function Header() {
           <HamburgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
         </div>
         <div className={HeaderStyle.auth}>
-          <Link to="/login">
-            <p className={HeaderStyle.login}>로그인</p>
-          </Link>
-          <Link to="/signupform">
-            <p className={HeaderStyle.signup}>회원가입</p>
-          </Link>
-          <Link to="/mypage">
-            <p className={HeaderStyle.mypage}>마이페이지</p>
-          </Link>
+          {isLoggedIn ? (
+            <>
+              <p onClick={handleLogout} className={HeaderStyle.logout}>
+                로그아웃
+              </p>
+              <Link to="/mypage">
+                <p className={HeaderStyle.mypage}>마이페이지</p>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/login">
+                <p className={HeaderStyle.login}>로그인</p>
+              </Link>
+              <Link to="/signupform">
+                <p className={HeaderStyle.signup}>회원가입</p>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </div>
--- react/src/pages/Auth/LoginPage.jsx
+++ react/src/pages/Auth/LoginPage.jsx
@@ -2,21 +2,75 @@ import { Link } from "react-router-dom";
 import img from "../../assets/images/loginBg1.png";
 import styles from "./LoginPage.module.css";
 
-const LoginPage = () => {
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
+function LoginPage() {
+  const [userId, setUserId] = useState('');
+  const [userPw, setUserPw] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const navigate = useNavigate();
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    setErrorMsg("");
+
+    try {
+      const response = await axios.post('http://localhost:8080/api/v1/auth/sign-in', {
+        userId,
+        userPw,
+      });
+      const token = response.data.token;
+      alert('로그인 성공!');
+      localStorage.setItem('token', token);
+      navigate("/");
+       
+    } catch (err) {
+    console.error(err);
+
+    if (err.response && err.response.data) {
+      const data = err.response.data;
+      if (data.code === "SF") {
+        setErrorMsg("존재하지 않는 아이디입니다.");
+      } else if (data.code === "IP") {
+        setErrorMsg("비밀번호가 올바르지 않습니다.");
+      } else {
+        setErrorMsg("로그인 실패: 알 수 없는 오류입니다.");
+      }
+    } else {
+      setErrorMsg("서버와 통신할 수 없습니다.");
+    }
+  }
+};
+
   return (
+    <form onSubmit={handleSignIn}>
     <div className={styles.loginWrap}>
       <div className={styles.backImg}>
         <img className={styles.img1} src={img} alt="Login Background" />
       </div>
       <div className={styles.loginBox}>
+        <Link to="/">
         <h1 className={styles.logo}>MIN:UTE</h1>
+        </Link>
         <div className="formGroup">
-          <input type="text" placeholder="UserName" className={styles.input} />
+          <input type="text" 
+                 value={userId}
+                 placeholder="Id" 
+                 onChange={(e) => setUserId(e.target.value)}
+                 className={styles.input} />
           <input
             type="password"
+            value={userPw}
             placeholder="Password"
-            className={styles.input}
+            onChange={(e) => setUserPw(e.target.value)}
+            className={styles.input1}
+            required
           />
+          <div className={styles.err}>
+            {errorMsg}
+          </div>
           <button className={styles.btn}>Login</button>
         </div>
         <div className={styles.links}>
@@ -26,6 +80,8 @@ const LoginPage = () => {
         </div>
       </div>
     </div>
+    </form>
+
   );
 };
 
