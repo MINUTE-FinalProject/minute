b72082d3a62e054c53304b8c402118caf9fa4d6b
bbwansu <orm6150@gmail.com>
2025-05-21 23:33:43 +0900

    #76 0521 23:33 vite 프록시 설정

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,7 +1,16 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // '/api'로 시작하는 모든 요청을 http://localhost:8080 (Spring Boot 서버)으로 전달합니다.
+      '/api': {
+        target: 'http://localhost:8080', // Spring Boot API 서버 주소
+        changeOrigin: true, // HTTP 요청 헤더의 Host 값을 대상 서버의 값으로 변경합니다.
+      }
+    }
+  }
 })
